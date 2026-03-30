0763e6d90d1b17732bf820068833b37056b68252
josemanueljaimemorales <josemanueljaimemorales@gmail.com>
2026-03-30 05:04:25 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
-console.log("APP INICIANDO");
-
 let dataGlobal = [];
 
 fetch('data/basicos.json')
-  .then(res => {
-    if (!res.ok) throw new Error("No se pudo cargar JSON");
-    return res.json();
-  })
+  .then(res => res.json())
   .then(data => {
-    console.log("DATA OK", data.length);
     dataGlobal = data;
     mostrarAparatos();
-  })
-  .catch(err => {
-    document.getElementById("contenido").innerHTML =
-      "<h2 style='color:red'>Error cargando datos</h2>";
-    console.error(err);
   });
 
 function mostrarAparatos() {
   const cont = document.getElementById("contenido");
-
   const aparatos = [...new Set(dataGlobal.map(d => d.aparato))];
 
-  cont.innerHTML = aparatos.map(a =>
-    `<button onclick="mostrarDificultad('${a}')">${a}</button>`
-  ).join('');
+  let html = "";
+  for (let i = 0; i < aparatos.length; i++) {
+    html += "<button onclick=\"mostrarDificultad('" + aparatos[i] + "')\">" + aparatos[i] + "</button>";
+  }
+
+  cont.innerHTML = html;
 }
 
 function mostrarDificultad(aparato) {
   const cont = document.getElementById("contenido");
+  const dificultades = ["A","B","C","D"];
 
-  const dificultades = ["A","B","C","D"]; // FORZADAS
+  let html = "<h2>" + aparato + "</h2>";
 
-  cont.innerHTML = `<h2>${aparato}</h2>` +
-    dificultades.map(d =>
-      `<button onclick="mostrarTabla('${aparato}','${d}')">${d}</button>`
-    ).join('');
+  for (let i = 0; i < dificultades.length; i++) {
+    html += "<button onclick=\"mostrarTabla('" + aparato + "','" + dificultades[i] + "')\">" + dificultades[i] + "</button>";
+  }
+
+  html += "<br><br><button onclick='mostrarAparatos()'>⬅ Volver</button>";
+
+  cont.innerHTML = html;
 }
 
 function mostrarTabla(aparato, dificultad) {
@@ -46,13 +41,19 @@ function mostrarTabla(aparato, dificultad) {
     .filter(d => d.aparato === aparato && d.dificultad === dificultad)
     .sort((a,b) => a.orden - b.orden);
 
-  let html = `<h2>${aparato} - ${dificultad}</h2><table>`;
+  let html = "<h2>" + aparato + " - " + dificultad + "</h2>";
 
-  elementos.forEach(e => {
-    html += `<tr><td>${e.orden}</td><td>${e.elemento}</td></tr>`;
-  });
+  if (elementos.length === 0) {
+    html += "<p>No hay elementos aún</p>";
+  } else {
+    html += "<table>";
+    for (let i = 0; i < elementos.length; i++) {
+      html += "<tr><td>" + elementos[i].orden + "</td><td>" + elementos[i].elemento + "</td></tr>";
+    }
+    html += "</table>";
+  }
 
-  html += "</table><br><button onclick='mostrarAparatos()'>Volver</button>";
+  html += "<br><button onclick=\"mostrarDificultad('" + aparato + "')\">⬅ Volver</button>";
 
   cont.innerHTML = html;
 }
